a5281fb8dd8468cb6fbb370741ba65b2ead1d330
Anton Oparin <mavarazy@gmail.com>
2017-07-06 20:47:31 +0200

    #18 workaround

--- src/engine/predicate/conditionsMeet.js
+++ src/engine/predicate/conditionsMeet.js
@@ -6,17 +6,19 @@ export default function conditionsMeet(conditions, formData) {
   if (!isObject(conditions) || !isObject(formData)) {
     toError(`Rule ${conditions} with ${formData} can't be processed`);
   }
-  return Object.keys(conditions).every(ref => {
-    if (ref === "or") {
-      return conditions[ref].some(subRule => conditionsMeet(subRule, formData));
-    } else if (ref === "and") {
-      return conditions[ref].every(subRule =>
-        conditionsMeet(subRule, formData)
-      );
+  return Object.keys(conditions).every(conditionKey => {
+    if (conditionKey === "or") {
+      return conditions[conditionKey].some(sr => conditionsMeet(sr, formData));
+    } else if (conditionKey === "and") {
+      return conditions[conditionKey].every(sr => conditionsMeet(sr, formData));
     } else {
-      let refVal = selectn(ref, formData);
-      let refFieldRule = conditions[ref];
-      return checkField(refVal, refFieldRule);
+      let refVal = selectn(conditionKey, formData);
+      let refFieldRule = conditions[conditionKey];
+      if (Array.isArray(refVal)) {
+        return refVal.some(val => conditionsMeet(refFieldRule, val));
+      } else {
+        return checkField(refVal, refFieldRule);
+      }
     }
   });
 }
