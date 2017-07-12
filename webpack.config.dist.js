d903befb272bb7c19b33b1ffac5c018abfabd442
Anton Oparin <mavarazy@gmail.com>
2017-07-13 00:51:14 +0200

    Fixing configurations

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -2,38 +2,42 @@ var webpack = require("webpack");
 var path = require("path");
 
 module.exports = {
-    cache: true,
-    context: __dirname + "/src",
-    entry: "./index.js",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "/dist/",
-        filename: "form-with-predicate.js",
-        library: "JSONSchemaForm",
-        libraryTarget: "umd"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production")
-            }
-        })
-    ],
-    devtool: "source-map",
-    externals: {
-        react: {
-            root: "React",
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "react"
-        }
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ["babel-loader"],
-            }
-        ]
+  cache: true,
+  context: __dirname + "/src",
+  entry: {
+    main: "./index.js",
+    "engine/SimplifiedRuleEngineFactory": './engine/SimplifiedRuleEngineFactory',
+    "engine/CacheControlEngineFactory": "./engine/CacheControlEngineFactory",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/dist/",
+    filename: "[name].js",
+    library: "JSONSchemaForm",
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ],
+  devtool: "source-map",
+  externals: {
+    react: {
+      root: "React",
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react"
     }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+      }
+    ]
+  }
 };
