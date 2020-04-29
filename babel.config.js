f15327415956577f494e4e13d2ebd06241cc3162
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-04-29 17:34:49 +0100

    fix babel config to correctly interpret BABEL_ENV

---
+++ babel.config.js
@@ -0,0 +1,43 @@
+const BABEL_ENV = process.env.BABEL_ENV || process.env.NODE_ENV;
+
+const defaultPlugins = [
+  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-proposal-object-rest-spread',
+  // IE 11 support
+  '@babel/plugin-transform-object-assign'
+];
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: ['cjs', 'test'].includes(BABEL_ENV) ? 'commonjs' : false,
+        targets:
+          BABEL_ENV === 'test' ? { node: 'current' } : { browsers: 'defaults' }
+      }
+    ],
+    '@babel/preset-react'
+  ],
+  env: {
+    cjs: {
+      plugins: defaultPlugins,
+      ignore: ['test/**/*.js']
+    },
+    umd: {
+      plugins: defaultPlugins,
+      ignore: ['test/**/*.js']
+    },
+    es: {
+      plugins: [
+        ...defaultPlugins,
+        ['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }]
+      ],
+      ignore: ['test/**/*.js']
+    },
+    test: {
+      plugins: defaultPlugins,
+      ignore: []
+    }
+  }
+};
