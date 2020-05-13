c0d995ae89b0a100155cd2b106e31162244d8e41
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-05-13 18:10:00 +0100

    export applyRules as a named export as well

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import rulesRunner from "./rulesRunner";
 import { validateFields } from "./actions/validateAction";
 import { findRelSchemaAndField, findRelUiSchema } from "./utils";
 
-export { validateFields, findRelSchemaAndField, findRelUiSchema, rulesRunner };
-
+export { validateFields, findRelSchemaAndField, findRelUiSchema, rulesRunner, applyRules };
 export default applyRules;
