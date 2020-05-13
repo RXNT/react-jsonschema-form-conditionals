a01a6a508e5a0473974e7ea15e80943dd8f56954
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-05-13 19:06:02 +0100

    reformat

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import rulesRunner from "./rulesRunner";
 import { validateFields } from "./actions/validateAction";
 import { findRelSchemaAndField, findRelUiSchema } from "./utils";
 
-export { validateFields, findRelSchemaAndField, findRelUiSchema, rulesRunner, applyRules };
+export {
+  validateFields,
+  findRelSchemaAndField,
+  findRelUiSchema,
+  rulesRunner,
+  applyRules,
+};
 export default applyRules;
