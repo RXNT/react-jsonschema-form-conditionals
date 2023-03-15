dab6e061b6f9b99bdd21d4131d5af8cf2e8df9c0
Michael Cheever <58792711+cheeverm@users.noreply.github.com>
2023-03-15 16:20:23 -0400

    ED-NA - undid test changes and updated lint script

--- test/applyRules.render.test.js
+++ test/applyRules.render.test.js
@@ -1,58 +1,69 @@
-// import React from "react";
-// import Form from "react-jsonschema-form";
-// import Engine from "json-rules-engine-simplified";
-// import applyRules from "../src";
-// import sinon from "sinon";
-// import Adapter from "enzyme-adapter-react-16";
-// import { shallow, configure } from "enzyme";
+import React from "react";
+import Form from "react-jsonschema-form";
+import Engine from "json-rules-engine-simplified";
+import applyRules from "../src";
+import sinon from "sinon";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
-// const schema = {
-//   type: "object",
-//   title: "Encounter",
-//   required: [],
-//   properties: {
-//     firstName: { type: "string" },
-//     lastName: { type: "string" },
-//     name: { type: "string" },
-//   },
-// };
+const schema = {
+  type: "object",
+  title: "Encounter",
+  required: [],
+  properties: {
+    firstName: { type: "string" },
+    lastName: { type: "string" },
+    name: { type: "string" },
+  },
+};
 
-// const RULES = [
-//   {
-//     conditions: {
-//       firstName: "empty",
-//     },
-//     event: {
-//       type: "remove",
-//       params: {
-//         field: ["lastName", "name"],
-//       },
-//     },
-//   },
-// ];
+const RULES = [
+  {
+    conditions: {
+      firstName: "empty",
+    },
+    event: {
+      type: "remove",
+      params: {
+        field: ["lastName", "name"],
+      },
+    },
+  },
+];
 
-// test("Re render on formData change", () => {
-//   let ResForm = applyRules(schema, {}, RULES, Engine)(Form);
-//   const renderSpy = sinon.spy(ResForm.prototype, "render");
-//   const wrapper = shallow(<ResForm formData={{ firstName: "A" }} />);
+test("NO re render on same data", () => {
+  let ResForm = applyRules(schema, {}, RULES, Engine)(Form);
+  const renderSpy = sinon.spy(ResForm.prototype, "render");
+  const wrapper = shallow(<ResForm formData={{ firstName: "A" }} />);
 
-//   return new Promise(resolve => setTimeout(resolve, 500))
-//     .then(() => {
-//       wrapper.setProps({ formData: { firstName: "An" } });
-//       return new Promise(resolve => setTimeout(resolve, 500));
-//     })
-//     .then(() => {
-//       expect(renderSpy.calledTwice).toEqual(true);
-//     });
-// });
+  expect(renderSpy.calledOnce).toEqual(true);
 
-// test("Re render on non formData change change", () => {
-//   let ResForm = applyRules(schema, {}, RULES, Engine)(Form);
-//   const spy = sinon.spy(ResForm.prototype, "render");
-//   const wrapper = shallow(<ResForm formData={{ firstName: "A" }} some="A" />);
+  wrapper.setProps({ formData: { firstName: "A" } });
+  expect(renderSpy.calledOnce).toEqual(true);
+});
 
-//   wrapper.setProps({ formData: { firstName: "A" }, some: "B" });
-//   expect(spy.calledTwice).toEqual(true);
-// });
+test("Re render on formData change", () => {
+  let ResForm = applyRules(schema, {}, RULES, Engine)(Form);
+  const renderSpy = sinon.spy(ResForm.prototype, "render");
+  const wrapper = shallow(<ResForm formData={{ firstName: "A" }} />);
+
+  return new Promise(resolve => setTimeout(resolve, 500))
+    .then(() => {
+      wrapper.setProps({ formData: { firstName: "An" } });
+      return new Promise(resolve => setTimeout(resolve, 500));
+    })
+    .then(() => {
+      expect(renderSpy.calledTwice).toEqual(true);
+    });
+});
+
+test("Re render on non formData change change", () => {
+  let ResForm = applyRules(schema, {}, RULES, Engine)(Form);
+  const spy = sinon.spy(ResForm.prototype, "render");
+  const wrapper = shallow(<ResForm formData={{ firstName: "A" }} some="A" />);
+
+  wrapper.setProps({ formData: { firstName: "A" }, some: "B" });
+  expect(spy.calledTwice).toEqual(true);
+});
